0c4ece48551d59fcdca2c00624dc60a7b2b2e015
MAXBR3M <max.g.bremner@gmail.com>
2023-05-04 20:58:39 +1000

    Further inputting of attributed into the schema

--- Create_Group63.sql
+++ Create_Group63.sql
@@ -65,22 +65,87 @@ CREATE TABLE IF NOT EXISTS DRIVER(
     PRIMARY KEY (DriverLicenceNum)
 ) ENGINE=Innodb;
 
-CREATE TABLE IF NOT EXISTS LANGUAGE() ENGINE=Innodb;
+CREATE TABLE IF NOT EXISTS LANGUAGE(
+    LanguageCode CHAR(2),
+    LanguageName VARCHAR(50),
+    PRIMARY KEY (LanguageCode)
+) ENGINE=Innodb;
 
-CREATE TABLE IF NOT EXISTS COUNTRY() ENGINE=Innodb;
+CREATE TABLE IF NOT EXISTS COUNTRY(
+    CountryCode CHAR(2),
+    CountryName VARCHAR(50),
+    PRIMARY KEY (CountryCode) 
+) ENGINE=Innodb;
 
-CREATE TABLE IF NOT EXISTS OFFICIAL_ROLE() ENGINE=Innodb;
+CREATE TABLE IF NOT EXISTS OFFICIAL_ROLE(
+    RoleID CHAR(2),
+    RoleDescription VARCHAR(40),
+    PRIMARY KEY (RoleID)
+) ENGINE=Innodb;
 
-CREATE TABLE IF NOT EXISTS OFFICIAL() ENGINE=Innodb;
+CREATE TABLE IF NOT EXISTS OFFICIAL(
+    OfficialID CHAR(8),
+    OfficialRole CHAR(2),
+    CountryCode CHAR(2),
+    OfficialFirstName VARCHAR(40),
+    OfficialLastName VARCHAR(40),
+    PRIMARY KEY (OfficialID),
+    FOREIGN KEY (OfficialRole)
+        REFERENCES OFFICIAL_ROLE(RoleID),
+    FOREIGN KEY (CountryCode)
+        REFERENCES COUNTRY(CountryCode)
+) ENGINE=Innodb;
 
-CREATE TABLE IF NOT EXISTS COUNTRY_LANGUAGE() ENGINE=Innodb;
+CREATE TABLE IF NOT EXISTS COUNTRY_LANGUAGE(
+    CountryCode CHAR(2),
+    LanguageCode CHAR(2),
+    PercentageSpoken DECIMAL(5,2),
+    PRIMARY KEY (CountryCode, LanguageCode),
+    FOREIGN KEY (CountryCode)
+        REFERENCES COUNTRY(CountryCode),
+    FOREIGN KEY (LanguageCode)
+        REFERENCES LANGUAGE(LanguageCode)
+) ENGINE=Innodb;
 
-CREATE TABLE IF NOT EXISTS DRIVER_LANGUAGE() ENGINE=Innodb;
+CREATE TABLE IF NOT EXISTS DRIVER_LANGUAGE(
+    LanguageCode CHAR(2),
+    DriverLicenceNum CHAR(12),
+    DriverLanguageProf CHAR(1),
+    PRIMARY KEY (LanguageCode, DriverLicenceNum),
+    FOREIGN KEY (LanguageCode)
+        REFERENCES LANGUAGE(LanguageCode),
+    FOREIGN KEY (DriverLicenceNum)
+        REFERENCES DRIVER(DriverLicenceNum)
+) ENGINE=Innodb;
 
-CREATE TABLE IF NOT EXISTS OFFICIAL_LANGAUGE() ENGINE=Innodb;
+CREATE TABLE IF NOT EXISTS OFFICIAL_LANGAUGE(
+    OfficialID CHAR(8),
+    LanguageCode CHAR(2),
+    Off_Lang_Preference CHAR(1),
+    PRIMARY KEY (OfficialID, LanguageCode),
+    FOREIGN KEY (OfficialID)
+        REFERENCES OFFICIAL(OfficialID),
+    FOREIGN KEY (LanguageCode)
+        REFERENCES LANGUAGE(LanguageCode)
+) ENGINE=Innodb;
 
-CREATE TABLE IF NOT EXISTS LOCATION_TYPE() ENGINE=Innodb;
+CREATE TABLE IF NOT EXISTS LOCATION_TYPE(
+    LocTypeID CHAR(2),
+    LocTypeDescription VARCHAR(40),
+    PRIMARY KEY(LocTypeID)
+) ENGINE=Innodb;
 
-CREATE TABLE IF NOT EXISTS LOCATION() ENGINE=Innodb;
+CREATE TABLE IF NOT EXISTS LOCATION(
+    LocationID  INT AUTO_INCREMENT,
+    LocationTypeID CHAR(2),
+    StreetNo VARCHAR(5),
+    SteetName VARCHAR(50),
+    City VARCHAR(40),
+    State CHAR(3),
+    Postcode CHAR(4),
+    PRIMARY KEY (LocationID),
+    FOREIGN KEY (LocationTypeID)
+        REFERENCES LOCATION_TYPE(LocTypeID)
+) ENGINE=Innodb;
 
 CREATE TABLE IF NOT EXISTS TRIP() ENGINE=Innodb;

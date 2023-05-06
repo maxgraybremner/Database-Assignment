b382289e20879808697c151a252b8980c27ae7a7
MAXBR3M <max.g.bremner@gmail.com>
2023-05-07 07:59:56 +1000

    Create insertion statements templates for each table.

--- Insert_Group63.sql
+++ Insert_Group63.sql
@@ -1,3 +1,115 @@
 /* Insert_Group63.sql */
---leshgooooooo
---is it working ???
+
+USE CTSDB;
+
+INSERT INTO VEHICLE_TYPE(Veh_TypeID, Veh_Type_Description)
+    VALUES('se', 'sedan'),
+    ('', ''),
+    (),
+    (),
+    ();
+
+INSERT INTO VEHICLE()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO PASSENGER_VEHICLE()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO CARGO_VEHICLE()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO MAINTENANCE_REPAIR()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO DRIVER()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO LANGUAGE()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO COUNTRY()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO OFFICIAL_ROLE()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO OFFICIAL()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO COUNTRY_LANGUAGE()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO DRIVER_LANGUAGE()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO OFFICIAL_LANGAUGE()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO LOCATION_TYPE()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO LOCATION()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();
+
+INSERT INTO TRIP()
+    VALUES(),
+    (),
+    (),
+    (),
+    ();

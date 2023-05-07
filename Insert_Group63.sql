95cc79924d99e7a7f4ce3976cc01dffa17998be0
MAXBR3M <max.g.bremner@gmail.com>
2023-05-07 14:10:40 +1000

    Populated cargo_vehicle and maintenance_repair tables.

--- Insert_Group63.sql
+++ Insert_Group63.sql
@@ -12,12 +12,12 @@ INSERT INTO VEHICLE_TYPE(Veh_TypeID, Veh_Type_Description)
 
 INSERT INTO VEHICLE(VIN, Veh_TypeID, Veh_Make, Veh_Model, Veh_Year, Veh_Colour, Veh_RegoNum, Veh_Odometer, Veh_Availability, Veh_For_Passenger, Veh_For_Cargo)
     VALUES('WBA7T2C02LCD06343', 'se', 'BMW', '7 Series', '2020', 'silver', '921GQ3', 5984, 'y', 'y', 'n'),
-    ('WBS3U920X0J966829', 'se', 'BMW', '3 Series', '2019', 'black', '241KST', 23984, 'n', 'y', 'n'),
+    ('WBS3U920X0J966829', 'se', 'BMW', '3 Series', '2019', 'black', '241KST', 23984, 'y', 'y', 'n'),
     ('WAUZZZ8V5G1048824', 'se', 'Audi', 'A5', '2018', 'black', '841GD5', 85846, 'n', 'y', 'n'),
     ('W1W8ED6Y7LP320845', 'va', 'Mercedes-Benz', 'Sprinter', '2019', 'white', '699KJB',47154, 'y', 'n', 'y'),
-    ('WDF9066352E036011', 'pm', 'Mercedes-Benz', 'Sprinter', '2019', 'white', '656GB5', 18126, 'n', 'y', 'n'),
+    ('WDF9066352E036011', 'pm', 'Mercedes-Benz', 'Sprinter', '2019', 'white', '656GB5', 18126, 'y', 'y', 'n'),
     ('WDF9066352E086788', 'pm', 'Mercedes-Benz', 'Sprinter', '2019', 'black', '886CH8', 23156, 'n', 'y', 'n'),
-    ('JHDFM8JR1XXX10152', 'bt', 'Hino', '300 Series', '2016', 'white', 'XQZ4CH', 125684, 'n', 'n', 'y'),
+    ('JHDFM8JR1XXX10152', 'bt', 'Hino', '300 Series', '2016', 'white', 'XQZ4CH', 125684, 'y', 'n', 'y'),
     ('JHDFC2AG1XXX10349', 'bt', 'Hino', '300 Series', '2016', 'white', '235EDO', 184693, 'n', 'n', 'y'),
     ('JHDFC2AG1XXX10594', 'bt', 'Hino', '300 Series', '2017', 'white', '331TFT', 155811, 'y', 'n', 'y'),
     ('MR0HA3CD400387900', 'uv', 'Toyota', 'Hilux', '2017', 'white', '233XOR', 90545, 'y', 'y', 'y'),
@@ -41,19 +41,21 @@ INSERT INTO PASSENGER_VEHICLE(VIN, Pass_seat_capacity)
     ('KMHH351EMNU122345', 5);
 
 
-INSERT INTO CARGO_VEHICLE()
-    VALUES(),
-    (),
-    (),
-    (),
-    ();
+INSERT INTO CARGO_VEHICLE(VIN, Carg_cfeet_capacity, Carg_tons_capacity)
+    VALUES('W1W8ED6Y7LP320845', 71, 4.1),
+    ('JHDFM8JR1XXX10152', 89, 4.5),
+    ('JHDFC2AG1XXX10349', 89, 4.5),
+    ('JHDFC2AG1XXX10594', 89, 4.5),
+    ('MR0HA3CD400387900', 43, 3.2),
+    ('MR0HA3CD400355234', 43, 3.2),
+    ('MR0HA3CD400323564', 43, 3.2);
 
-INSERT INTO MAINTENANCE_REPAIR()
-    VALUES(),
-    (),
-    (),
-    (),
-    ();
+INSERT INTO MAINTENANCE_REPAIR(VIN, OdometerReading, MR_Type, MR_Date, MR_Cost, MR_Detail)
+    VALUES('WBS3U920X0J966829', 23984, 'm', '2023-06-02', 1163.75, '20,000km inspection'),
+    ('WDF9066352E036011', 18126, 'r', '2023-05-18', 240,'Air-Conditioning is not cold and engine fan full speed'),
+    ('JHDFM8JR1XXX10152', 125684, 'm', '2023-05-29', 831, '125,000km service and investogate shudder when changin gears'),
+    ('MR0HA3CD400355234', 84569, 'r', '2023-05-11', 320, 'Investigate intermittently blowing white smoke while driving on the highway'),
+    ('KMHH351EMNU176947', 32565, 'm', '2023-05-12', 596.99, '30,000km service and an issue during cold start.');
 
 INSERT INTO DRIVER()
     VALUES(),

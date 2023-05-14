01ae37e35a8eede2dda41f4543a7e918b0106fe8
MisterTriangleMan <elliot.backstrom@gmail.com>
2023-05-14 20:36:08 +1000

    Commented out the second half of the question

--- Booking_Group63.sql
+++ Booking_Group63.sql
@@ -13,3 +13,16 @@
     Expected Start: 10:00AM
     Expected End: 10:45AM
 */
+/*  Booking 2: (associated with the above booking)
+    Vehicle Type: Sedan
+    VIN: SANFDAE33U1286116
+    Start Odo: 126982 KM
+    End Odo: 127119 KM
+    Driver Fname: Md
+    Driver Lname: Polash
+    Language Prof: English 4
+    Security Clearance: Level 2
+    License Number: 098675532
+    Actual Start: 10:15AM
+    Actual End: 11:55AM
+*/

7f4a0244d1c688bfd9c755020e6d9916da84f0fe
MAXBR3M <max.g.bremner@gmail.com>
2023-05-14 19:38:04 +1000

    Added Questions to Booking SQL file

--- Booking_Group63.sql
+++ Booking_Group63.sql
@@ -1,1 +1,18 @@
 /* Booking_Group63.sql */
+/*Q1 An Official named Daniel Ortega from Spain, having OfficialID SPN99745, 
+wants to make a booking. He speaks in both Spanish and English, Spanish 
+being his highest preferred language. Spanish is currently spoken by 94% of 
+the total population in Spain. He will play the role of a ‘Judge’ in the 
+games. He wants to travel from 16 Brendan Thorne Place, Gold Coast 
+(Rydges Gold Coast) to 117 Rubicon Crescent, Surfers Paradise (Swimming 
+Pool) on April 9, 2023. His expected start time is 10:00 am and end time is 
+10:45 am.*/
+
+/*Q2 The above trip is then planned to complete using the Sedan vehicle having 
+VIN number SANFDAE33U1286116. The starting odometer reading for 
+the vehicle is 126,982 KM. A driver with the highest preferred language of 
+the official is not available, so the driver Md Polash with language 
+proficiency level 4 in English and security clearance level 2 is booked. His 
+licence number is 098675532. The trip actually starts at 10:15 am and ends 
+at 11:55 am on the same date. At the end of the trip the odometer reading is 
+recorded as 127,119 KM*/
